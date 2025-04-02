7007a52a6ec8ee1a4c86a04ef05551d81a0cd9a5
Alvaro <alvarorodmi7@gmail.com>
2025-04-03 00:15:18 +0100

    Header component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './component/header/header.component';
+import {CarouselComponent} from './component/carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
